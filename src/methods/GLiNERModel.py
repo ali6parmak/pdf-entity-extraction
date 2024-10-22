2345f1e0ca8e5e700efdc1cafd794d649dbf6465
Ali Altıparmak <ali@huridocs.org>
2024-10-22 18:51:11 +0300

    Update GLiNER method

--- src/methods/GLiNERModel.py
+++ src/methods/GLiNERModel.py
@@ -1,12 +1,16 @@
+import json
+
 from gliner import GLiNER
 from pdf_features.Rectangle import Rectangle
 from data_model.EntityBox import EntityBox
 from data_model.WordBox import WordBox
 from methods.NERTransformerModel import NERTransformerModel
 
+
 def print_with_line_breaks(text, line_length=150):
     for i in range(0, len(text), line_length):
-        print(text[i:i+line_length])
+        print(text[i : i + line_length])
+
 
 class GLiNERModel(NERTransformerModel):
     def __init__(self, model_name: str, show_logs: bool = False):
@@ -14,6 +18,28 @@ class GLiNERModel(NERTransformerModel):
         self.model_name = model_name
         self.classifier = GLiNER.from_pretrained(model_name)
         self.show_logs = show_logs
+
+    @staticmethod
+    def find_unique_dicts(dict_list: list[dict]) -> list[dict]:
+        def dict_hash(d: dict) -> str:
+            return json.dumps(d, sort_keys=True)
+
+        unique_dicts = {dict_hash(d): d for d in dict_list}
+        return list(unique_dicts.values())
+
+    @staticmethod
+    def remove_overlapping_entities(entities):
+        sorted_entities = sorted(entities, key=lambda x: (x["start_index"], -len(x["text"])))
+
+        result = []
+        last_end = -1
+
+        for entity in sorted_entities:
+            if entity["start_index"] >= last_end:
+                result.append(entity)
+                last_end = entity["end_index"]
+
+        return result
 
     def process_segment(self, pdf_words, segment_box, total_entity_count) -> list[EntityBox]:
         segment_bounding_box = Rectangle.from_width_height(
@@ -26,37 +52,24 @@ class GLiNERModel(NERTransformerModel):
 
         labels = ["date"]
         entities = []
-        full_text = " ".join([wb.text for wb in word_boxes_in_segment])
-        previous_chunk_index_end = 0
-        chunk_text = ""
-        for word_box in word_boxes_in_segment:
-            if len(chunk_text) + len(word_box.text) < 375:
-                chunk_text += word_box.text + " "
-                continue
-            chunk_text += word_box.text
+        window_size = 20
+        slide_size = 10
+        last_slide_end_index = 0
 
-            chunk_entities = self.classifier.predict_entities(chunk_text, labels)
+        for i in range(0, len(word_boxes_in_segment), slide_size):
+            window_word_boxes = word_boxes_in_segment[i : i + window_size]
+            window_text = " ".join([wb.text for wb in window_word_boxes])
+            window_entities = self.classifier.predict_entities(window_text, labels)
 
-            for entity in chunk_entities:
-                entity["start"] += previous_chunk_index_end
-                entity["end"] += previous_chunk_index_end
+            for entity in window_entities:
+                entity["start"] += last_slide_end_index
+                entity["end"] += last_slide_end_index
 
-            previous_chunk_index_end += len(chunk_text) + 1
+            slide_window_boxes = word_boxes_in_segment[i : i + slide_size]
+            slide_text = " ".join([wb.text for wb in slide_window_boxes])
+            last_slide_end_index += len(slide_text) + 1
 
-            entities.extend(chunk_entities)
-            chunk_text = ""
-
-        if chunk_text:
-            chunk_entities = self.classifier.predict_entities(chunk_text, labels)
-
-            for entity in chunk_entities:
-                entity["start"] += previous_chunk_index_end
-                entity["end"] += previous_chunk_index_end
-
-            previous_chunk_index_end += len(chunk_text) + 1
-
-            entities.extend(chunk_entities)
-
+            entities.extend(window_entities)
 
         aggregated_entities = [
             {
@@ -67,6 +80,9 @@ class GLiNERModel(NERTransformerModel):
             }
             for entity in entities
         ]
+
+        aggregated_entities = self.find_unique_dicts(aggregated_entities)
+        aggregated_entities = self.remove_overlapping_entities(aggregated_entities)
 
         print("PAGE: ", segment_box["page_number"])
         print_with_line_breaks(" ".join([wb.text for wb in word_boxes_in_segment]))
