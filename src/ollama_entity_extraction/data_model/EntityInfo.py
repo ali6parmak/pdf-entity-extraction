39e712abf27a4b8b20764a375b2dfa6e2555e59c
Ali Altıparmak <ali6parmakk@gmail.com>
2024-11-26 17:10:27 +0300

    Add EntityInfo

--- src/ollama_entity_extraction/data_model/EntityInfo.py
+++ src/ollama_entity_extraction/data_model/EntityInfo.py
@@ -1,2 +1,17 @@
+from dataclasses import dataclass, field
 
 
+@dataclass
+class EntityInfo:
+    pages: list[str] = field(default_factory=list)
+    mentions: list[str] = field(default_factory=list)
+    mention_starts: list[int] = field(default_factory=list)
+    mention_ends: list[int] = field(default_factory=list)
+    segment_numbers: list[int] = field(default_factory=list)
+
+    def extend(self, other: "EntityInfo"):
+        self.pages.extend(other.pages)
+        self.mentions.extend(other.mentions)
+        self.mention_starts.extend(other.mention_starts)
+        self.mention_ends.extend(other.mention_ends)
+        self.segment_numbers.extend(other.segment_numbers)
